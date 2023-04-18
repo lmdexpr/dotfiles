61c26327b02888ccb3a497d0c90fa079927b1fcd
lmdexpr <lmdexpr@gmail.com>
2023-04-18 16:24:15 +0900

    delete unnecessary configuration

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,18 +1,5 @@
 local wezterm = require 'wezterm';
 
-wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-  local title = wezterm.truncate_right(utils.basename(tab.active_pane.foreground_process_name), max_width)
-  if title == "" then
-    title = wezterm.truncate_right(
-    utils.basename(utils.convert_home_dir(tab.active_pane.current_working_dir)),
-    max_width
-    )
-  end
-  return {
-    { Text = tab.tab_index + 1 .. ":" .. title },
-  }
-end)
-
 local home = os.getenv("HOME")
 
 return {
