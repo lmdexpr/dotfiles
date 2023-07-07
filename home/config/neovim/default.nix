fd28634f72eae0ea8e6a491e074d4131538dd840
lmdexpr <lmdexpr@gmail.com>
2023-07-07 15:40:56 +0900

    treesitter

--- home/config/neovim/default.nix
+++ home/config/neovim/default.nix
@@ -1,5 +1,28 @@
 { inputs, pkgs, lib, ... }:
 
+let 
+  buildGrammar = pkgs.callPackage "${inputs.nixpkgs}/pkgs/development/tools/parsing/tree-sitter/grammar.nix" {};
+
+  genAttrs' = values: f: with lib; listToAttrs (map (v: nameValuePair (f v) v) values);
+  grammars = with lib;
+    genAttrs' pkgs.vimPlugins.nvim-treesitter.withAllGrammars.passthru.dependencies
+    (v: replaceStrings ["vimplugin-treesitter-grammar-"] ["tree-sitter-"] v.name);
+
+  parserDir = with lib;
+    pkgs.linkFarm
+    "treesitter-parsers"
+    (mapAttrsToList
+      (n: v:
+      let name = "${replaceStrings ["-"] ["_"] (removePrefix "tree-sitter-" n)}.so"; in {
+        inherit name;
+        path =
+          # nvim-treesitter's grammars are inside a "parser" directory, which sucks
+          if hasPrefix "vimplugin-treesitter" v.name
+          then "${v}/parser/${name}"
+          else "${v}/parser";
+      })
+     grammars);
+in
 {
   programs.neovim = {
     enable   = true;
@@ -17,6 +40,9 @@
     };
     "nvim/snippet" = {
       source = ./nvim/snippet; recursive = true;
+    };
+    "nvim/parser" = {
+      source = "${parserDir}"; recursive = true;
     };
   };
 }
