abf030396487df653e46ca6688f2d69115972a50
lmdexpr <lmdexpr@gmail.com>
2023-07-07 15:31:10 +0900

    reset neovim config & migrate preztorc to nix

--- home/config/neovim/default.nix
+++ home/config/neovim/default.nix
@@ -1,41 +1,5 @@
 { inputs, pkgs, lib, ... }:
 
-let 
-  sources = pkgs.callPackage _sources/generated.nix {};
-
-  # Grammar builder function
-  buildGrammar = pkgs.callPackage "${inputs.nixpkgs}/pkgs/development/tools/parsing/tree-sitter/grammar.nix" {};
-
-  # Build grammars that were fetched using nvfetcher
-  generatedGrammars = with lib;
-    mapAttrs (n: v:
-      buildGrammar {
-        language = removePrefix "tree-sitter-" n;
-        inherit (v) version src;
-      }) (filterAttrs (n: _: hasPrefix "tree-sitter-" n) sources);
-
-  genAttrs' = values: f: with lib; listToAttrs (map (v: nameValuePair (f v) v) values);
-  # Attrset of grammars built using nvim-treesitter's lockfile
-  grammars' = with lib;
-    genAttrs' pkgs.vimPlugins.nvim-treesitter.withAllGrammars.passthru.dependencies
-    (v: replaceStrings ["vimplugin-treesitter-grammar-"] ["tree-sitter-"] v.name);
-  grammars = grammars' // generatedGrammars;
-
-  parserDir = with lib;
-    pkgs.linkFarm
-    "treesitter-parsers"
-    (mapAttrsToList
-      (n: v:
-      let name = "${replaceStrings ["-"] ["_"] (removePrefix "tree-sitter-" n)}.so"; in {
-        inherit name;
-        path =
-          # nvim-treesitter's grammars are inside a "parser" directory, which sucks
-          if hasPrefix "vimplugin-treesitter" v.name
-          then "${v}/parser/${name}"
-          else "${v}/parser";
-      })
-     grammars);
-in
 {
   programs.neovim = {
     enable   = true;
@@ -53,9 +17,6 @@ in
     };
     "nvim/snippet" = {
       source = ./nvim/snippet; recursive = true;
-    };
-    "nvim/parser" = {
-      source = "${parserDir}"; recursive = true;
     };
   };
 }
--- home/config/zsh/default.nix
+++ home/config/zsh/default.nix
@@ -1,8 +1,44 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-  programs.zsh = { enable = true; };
+  programs.zsh = {
+    enable = true;
+    plugins = [
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.5.0";
+          sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
+        };
+      }
+    ];
+
+    prezto = {
+      enable = true;
+      pmodules = [
+        "environment"
+        "terminal"
+        "editor"
+        "history"
+        "directory"
+        "spectrum"
+        "git"
+        "utility"
+        "syntax-highlighting"
+        "autosuggestions"
+        "nix"
+        "nix-shell"
+        "prompt"
+        "completion"
+      ];
+      editor.keymap = "vi";
+      git.submoduleIgnore = "all";
+      prompt.theme = "paradox";
+    };
+  };
 
   home.file.".zshrc".source = ./zshrc;
-  home.file.".zpreztorc".source = ./zpreztorc;
 }
