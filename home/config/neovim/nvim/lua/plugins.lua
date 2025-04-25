c460bd3cf92c22a9f7ffeafabc7caadbb95367a7
lmdexpr <lmdexpr+forgejo@gmail.com>
2025-04-25 15:25:31 +0900

    change: fern -> neotree

--- home/config/neovim/nvim/lua/plugins.lua
+++ home/config/neovim/nvim/lua/plugins.lua
@@ -60,16 +60,51 @@ return {
     dependencies = { 'nvim-telescope/telescope.nvim' ,'nvim-lua/plenary.nvim' },
   },
   {
-    'lambdalisue/fern.vim',
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
     dependencies = {
-      'lambdalisue/fern-git-status.vim',
-      'lambdalisue/nerdfont.vim',
-      'lambdalisue/fern-renderer-nerdfont.vim',
-      'lambdalisue/glyph-palette.vim'
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+      -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    lazy = false, -- neo-tree will lazily load itself
+    ---@module "neo-tree"
+    ---@type neotree.Config?
+    opts = {
+      filesystem = {
+        commands = {
+          avante_add_files = function(state)
+            local node = state.tree:get_node()
+            local filepath = node:get_id()
+            local relative_path = require('avante.utils').relative_path(filepath)
+
+            local sidebar = require('avante').get()
+
+            local open = sidebar:is_open()
+            -- ensure avante sidebar is open
+            if not open then
+              require('avante.api').ask()
+              sidebar = require('avante').get()
+            end
+
+            sidebar.file_selector:add_selected_file(relative_path)
+
+            -- remove neo tree buffer
+            if not open then
+              sidebar.file_selector:remove_selected_file('neo-tree filesystem [1]')
+            end
+          end,
+        },
+        window = {
+          mappings = {
+            ['oa'] = 'avante_add_files',
+          },
+        },
+      },
     },
     config = function ()
-      vim.g["fern#renderer"] = 'nerdfont'
-      vim.keymap.set('n', '<C-e>', ':<C-u>Fern . -reveal=% -drawer -toggle<CR>', { noremap = true, silent = true })
+      vim.keymap.set('n', '<C-e>', ':<C-u>Neotree<CR>', { noremap = true, silent = true })
     end
   },
   {
@@ -338,7 +373,7 @@ return {
           lualine_a = {}, lualine_b = {}, lualine_c = {},
           lualine_x = {}, lualine_y = {}, lualine_z = {},
         },
-        extension = {'lazy', 'fern'},
+        extension = {'neo-tree'},
       }
     end
   },
