04847206c5ea66147c0e7cbdfcfc4dddac4ef4a8
lmdexpr <lmdexpr@gmail.com>
2025-04-29 10:42:56 +0900

    add ocaml_mlx

--- home/config/neovim/nvim/lua/plugins.lua
+++ home/config/neovim/nvim/lua/plugins.lua
@@ -151,6 +151,16 @@ return {
       }
     end
   },
+  {
+    'ocaml-mlx/ocaml_mlx.nvim',
+    dependencies = {
+      'neovim/nvim-lspconfig',
+      'nvim-treesitter/nvim-treesitter'
+    },
+    config = function ()
+      require 'ocaml_mlx'
+    end
+  },
   
   {
     "zbirenbaum/copilot.lua",
