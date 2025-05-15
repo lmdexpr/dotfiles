057599f27f19951beb84493ef6663bc4e32c5375
lmdexpr <lmdexpr@gmail.com>
2025-05-15 22:50:01 +0900

    nord vim

--- home/config/neovim/nvim/lua/plugins.lua
+++ home/config/neovim/nvim/lua/plugins.lua
@@ -94,7 +94,6 @@ return {
     config = function()
       require('lualine').setup {
         options = {
-          theme = "catppuccin",
           icons_enabled = true,
           section_separators = { left = '', right = ''},
           component_separators = { left = '|', right = '|' },
@@ -558,46 +557,67 @@ return {
   --     require("everforest").load()
   --   end
   -- }
-  { 
-    "catppuccin/nvim",
-    name = "catppuccin",
+
+  -- { 
+  --   "catppuccin/nvim",
+  --   name = "catppuccin",
+  --   priority = 1000,
+  --   opts = {
+  --     flavour = "frappe", -- latte, frappe, macchiato, mocha
+  --     transparent_background = false,
+  --     integrations = {
+  --       cmp = true,
+  --       render_markdown = true,
+  --       nvimtree = true,
+  --       treesitter = true,
+  --       native_lsp = {
+  --         enabled = true,
+  --         virtual_text = {
+  --           errors = { "italic" },
+  --           hints = { "italic" },
+  --           warnings = { "italic" },
+  --           information = { "italic" },
+  --           ok = { "italic" },
+  --         },
+  --         underlines = {
+  --           errors = { "underline" },
+  --           hints = { "underline" },
+  --           warnings = { "underline" },
+  --           information = { "underline" },
+  --           ok = { "underline" },
+  --         },
+  --         inlay_hints = {
+  --           background = true,
+  --         },
+  --       },
+  --       telescope = {
+  --         enabled = true,
+  --       }
+  --     }
+  --   },
+  --   config = function(_, opts)
+  --     require("catppuccin").setup(opts)
+  --     vim.cmd.colorscheme "catppuccin"
+  --   end
+  -- }
+
+  {
+    'shaunsingh/nord.nvim',
+    lazy = false,
     priority = 1000,
-    opts = {
-      flavour = "frappe", -- latte, frappe, macchiato, mocha
-      transparent_background = false,
-      integrations = {
-        cmp = true,
-        render_markdown = true,
-        nvimtree = true,
-        treesitter = true,
-        native_lsp = {
-          enabled = true,
-          virtual_text = {
-            errors = { "italic" },
-            hints = { "italic" },
-            warnings = { "italic" },
-            information = { "italic" },
-            ok = { "italic" },
-          },
-          underlines = {
-            errors = { "underline" },
-            hints = { "underline" },
-            warnings = { "underline" },
-            information = { "underline" },
-            ok = { "underline" },
-          },
-          inlay_hints = {
-            background = true,
-          },
-        },
-        telescope = {
-          enabled = true,
-        }
-      }
-    },
-    config = function(_, opts)
-      require("catppuccin").setup(opts)
-      vim.cmd.colorscheme "catppuccin"
-    end
+    config = function()
+      vim.cmd[[colorscheme nord]]
+
+      -- Example config in lua
+      vim.g.nord_contrast = true
+      vim.g.nord_borders = false
+      vim.g.nord_disable_background = false
+      vim.g.nord_italic = false
+      vim.g.nord_uniform_diff_background = true
+      vim.g.nord_bold = false
+
+      -- Load the colorscheme
+      require('nord').set()
+    end,
   }
 }
