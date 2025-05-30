01ef090402624dc42a12ac7454a0328572bc14d6
lmdexpr <lmdexpr@gmail.com>
2025-05-30 12:06:32 +0900

    disable format on save

--- home/config/neovim/nvim/lua/plugins.lua
+++ home/config/neovim/nvim/lua/plugins.lua
@@ -577,7 +577,6 @@ return {
     },
     -- This will provide type hinting with LuaLS
     ---@module "conform"
-    ---@type conform.setupOpts
     opts = {
       -- Define your formatters
       formatters_by_ft = {
@@ -588,7 +587,7 @@ return {
         lsp_format = "fallback",
       },
       -- Set up format-on-save
-      format_on_save = { timeout_ms = 500 },
+      -- format_on_save = { timeout_ms = 500 },
       -- Customize formatters
       formatters = {
         shfmt = {
